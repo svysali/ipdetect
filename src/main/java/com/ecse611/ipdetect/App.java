1b6018b2dac88c779e74839849a062673a068143
svysali <shivashree.vaidhyamsubramanian@mail.mcgill.ca>
2019-02-06 14:02:11 -0500

    Initial commit, mvn java application template

---
+++ src/main/java/com/ecse611/ipdetect/App.java
@@ -0,0 +1,13 @@
+package com.ecse611.ipdetect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
