0588c1fcb6a4b75cca499c097996a5b4a8484f98
svysali <shivashree.vaidhyamsubramanian@mail.mcgill.ca>
2019-02-25 16:05:13 -0500

    Updated app

--- src/main/java/com/ecse611/ipdetect/App.java
+++ src/main/java/com/ecse611/ipdetect/App.java
@@ -1,13 +1,16 @@
 package com.ecse611.ipdetect;
-
+import com.ecse611.ipdetect.RepoParser;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args ) throws Exception
+	{
+		RepoParser rp = new RepoParser();
+		rp.setProject_name("accumulo");
+		rp.setRepo_path("/Users/svysali/Desktop/ecse611/assignment/repos/");
+		rp.printLogToCSV();
+	}
 }
